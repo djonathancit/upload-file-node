db23b950ffdcec6332645721c7fa7a5d5f3bc5f9
dmastrodi <dmastrodi@ciandt.com>
2020-06-05 17:02:59 -0300

    ajuste de upload

--- index.js
+++ index.js
@@ -1,106 +1,106 @@
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const _ = require('lodash');
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const _ = require("lodash");
 
 const app = express();
 
 // enable files upload
-app.use(fileUpload({
-    createParentPath: true
-}));
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
 
 //add other middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-//start app 
+//start app
 const port = process.env.PORT || 6000;
 
-app.listen(port, () => 
-  console.log(`App is listening on port ${port}.`)
-);
+app.listen(port, () => console.log(`App is listening on port ${port}.`));
 
+app.post("/upload", async (req, res) => {
+  try {
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: "No file uploaded",
+      });
+    } else {
+      let data = req.files.data;
 
+      console.log(1);
+      data.mv("./uploads/" + data.name);
 
-app.post('/upload', async (req, res) => {
-    try {
-        if(!req.files) {
-            res.send({
-                status: false,
-                message: 'No file uploaded'
-            });
-        } else {
-            
-            let data = req.files.data;
-            
-            console.log(1);
-            data.mv('./uploads/' + data.name);
-            console.log(2);
-            //send response
-            res.send({
-                status: true,
-                message: 'File is uploaded',
-                data: {
-                    name: data.name,
-                    mimetype: data.mimetype,
-                    size: data.size
-                }
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
+      var file = "./uploads/" + data.name;
+
+      console.log(2);
+
+      res.download(file);
+      //send response
+      // res.send({
+      //     status: true,
+      //     message: 'File is uploaded',
+      //     data: {
+      //         name: data.name,
+      //         mimetype: data.mimetype,
+      //         size: data.size
+      //     }
+      // });
     }
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
+app.post("/upload-photos", async (req, res) => {
+  try {
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: "No file uploaded",
+      });
+    } else {
+      let data = [];
 
-app.post('/upload-photos', async (req, res) => {
-    try {
-        if(!req.files) {
-            res.send({
-                status: false,
-                message: 'No file uploaded'
-            });
-        } else {
-            let data = []; 
-    
-            //loop all files
-            _.forEach(_.keysIn(req.files.photos), (key) => {
-                let photo = req.files.photos[key];
-                
-                //move photo to uploads directory
-                photo.mv('./uploads/' + photo.name);
+      //loop all files
+      _.forEach(_.keysIn(req.files.photos), (key) => {
+        let photo = req.files.photos[key];
 
-                //push file details
-                data.push({
-                    name: photo.name,
-                    mimetype: photo.mimetype,
-                    size: photo.size
-                });
-            });
-    
-            //return response
-            res.send({
-                status: true,
-                message: 'Files are uploaded',
-                data: data
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
+        //move photo to uploads directory
+        photo.mv("./uploads/" + photo.name);
+
+        //push file details
+        data.push({
+          name: photo.name,
+          mimetype: photo.mimetype,
+          size: photo.size,
+        });
+      });
+
+      //return response
+      res.send({
+        status: true,
+        message: "Files are uploaded",
+        data: data,
+      });
     }
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-
-app.get('/download', function(req, res){
-    console.log(1);
-    var fileName = req.query.fileName;
-    console.log(2);
-    const file = `./uploads/${fileName}`;
-    console.log(3);
-    res.download(file); // Set disposition and send it.
-  });
+app.get("/download", function (req, res) {
+  console.log(1);
+  var fileName = req.query.fileName;
+  console.log(2);
+  const file = `./uploads/${fileName}`;
+  console.log(3);
+  res.download(file); // Set disposition and send it.
+});
